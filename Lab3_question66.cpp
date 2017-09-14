a61c984d8ecf1fa851bf2350e4169872abc93a9e
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-15 01:32:15 +0530

    Create Lab3_question66.cpp

---
+++ Lab3_question66.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main() {
+	long a,b=0,c,d,e,f=0,g=0,h=1;
+	cout<<" enter decimal number ";
+	cin>>a;
+	c=a;
+	while(a>0)
+	{
+		d=a%10;
+		f=f+d*pow(8,g);
+		a=a/10;
+		++g;
+		}
+		while(f>0)
+		{
+			e=f%2; 
+			b=b+e*h;
+			h=h*10;
+			f=f/2;
+			}
+			cout<<c<<" \nbinary form: "<<b;
+}
