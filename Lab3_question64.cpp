aeb86a5c833f4c802a1a960ef0d6535ff1915716
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-15 01:03:20 +0530

    Update Lab3_question64.cpp

--- Lab3_question64.cpp
+++ Lab3_question64.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main() {
+	long a,b=0,c,d,e,f=0,g=0,h=1;
+	cout<<" enter an octal number ";
+	cin>>a;
+	c=a;
+	while(a>0)
+	{
+		d=a%10;
+		f=f+d*pow(8,g);
+		a=a/10;
+		++g;
+		}
+		cout<<c<<" decimal form: "<<f;
+}
 
