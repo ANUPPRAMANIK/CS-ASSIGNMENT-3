24637cdcf2ec4b8e87534f04bbf6f68f2b413ea7
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-15 19:05:19 +0530

    Update Lab3_question71.cpp

--- Lab3_question71.cpp
+++ Lab3_question71.cpp
@@ -1,26 +1,32 @@
 #include<iostream>
-#include<string.h>
-#include<cmath>
 using namespace std;
-int a(char b[]) {
-	int c,d,e=0;
-	for(d=0;b[d]!='\0';d++);
-	for(c=0;b[c]!='\0';c++,d--)
-	{
-		if(b[c]>='0'&&b[c]<='9')
-		e=e+(b[c]-'0')*pow(16,d-1);
-		if(b[c]>='A'&&b[c]<='F')
-		e=e+(b[c]-55)*pow(16,d-1); 
-		if(b[c]>='a'&&b[c]<='f') 
-		e=e+(b[c]-87)*pow(16,d-1);
-		}
-		return e;
-		}
 main() {
-	char b[20],c; 
-	int n;
-	cout<<" enter a hexadecimal number ";
-	cin>>b;
-	cout<<" \nequivalent decimal value: "<<a(b);
-	return 0;
-}
+	int a,b;
+	char c[17],d[17];
+	int e=0;
+	cout<<" enter a binary number(maximum 16 bits) ";
+	cin>>c;
+	for(a=0;c[a]!='\0';a++);
+	for(b=(a-1);b>=0;b--)
+	{
+		if(e==0)
+		{
+			if(c[b]=='0')
+			d[b]='0';
+			else
+			{
+				d[b]='1';
+				e=1;
+				}
+				}
+				else
+				{
+					if(c[b]=='0')
+					d[b]='1';
+					else
+					d[b]='0';
+					}
+					}
+					cout<<" \ntwo's complement: "<<d;
+					return 0;
+					}
